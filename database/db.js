aec3875b78fb50626e205f479260cfb2cb395dd8
francisco <franciscopalenciaa@gmail.com>
2022-12-18 18:25:41 -0500

    se agrega nube

--- database/db.js
+++ database/db.js
@@ -1,8 +1,13 @@
 import {Sequelize} from 'sequelize'
+import {MYSQLHOST, MYSQLDATABASE, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER} from '../config.js'
 
-const db =new Sequelize('database_Blogs', 'root', '',{
-    host:'localhost',
+const db =new Sequelize('database_Blogs',{
+    host: MYSQLHOST,
     dialect:'mysql',
+    user: MYSQLUSER,
+    password: MYSQLPASSWORD,
+    port: MYSQLPORT,
+    database:MYSQLDATABASE
     
 })
 
