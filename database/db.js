c21deface22bac2cbf9845b8e18873c286947cc7
francisco <franciscopalenciaa@gmail.com>
2022-12-19 09:42:40 -0500

    cambios

--- database/db.js
+++ database/db.js
@@ -1,11 +1,15 @@
 import {Sequelize} from 'sequelize'
-import {MYSQLHOST, MYSQLDATABASE} from '../config.js'
+
+import {MYSQLHOST, MYSQLDATABASE, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER} from '../config.js'
 
 const db = new Sequelize(MYSQLDATABASE, 'root', "",{
     host: MYSQLHOST,
     dialect:'mysql',
-    
-    
+    password: MYSQLPASSWORD,
+    port: MYSQLPORT,
+    database: MYSQLDATABASE,
+    username: MYSQLUSER
+      
 })
 
 export default db
